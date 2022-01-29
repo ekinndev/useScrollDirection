c593a93222babbba88c3033aaa3e24edbc7cdb8b
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2022-01-30 01:28:20 +0300

    Change file name as index.js

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 import { useEffect, useState } from 'react';
 
-function useScrollDirection() {
+function debounce(func, wait, immediate) {
+  var timeout;
+
+  return function executedFunction() {
+    var context = this;
+    var args = arguments;
+
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+
+    var callNow = immediate && !timeout;
+
+    clearTimeout(timeout);
+
+    timeout = setTimeout(later, wait);
+
+    if (callNow) func.apply(context, args);
+  };
+}
+
+function useScrollDirection(time = 100) {
   if (typeof window === 'undefined') return;
   const isClient = typeof window === 'object';
 
@@ -24,8 +46,10 @@ function useScrollDirection() {
       });
     }
 
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
+    const debouncedHandleScroll = debounce(handleScroll, time);
+
+    window.addEventListener('scroll', debouncedHandleScroll);
+    return () => window.removeEventListener('scroll', debouncedHandleScroll);
   }, []);
 
   return scrollDirection;
